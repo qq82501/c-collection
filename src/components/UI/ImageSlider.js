6db944f28b819115baf12dbe216944930894167d
qq82501 <qq82501@gmail.com>
2023-01-29 01:42:20 +0800

    enable switching image by swiping on carousel

--- src/components/UI/ImageSlider.js
+++ src/components/UI/ImageSlider.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import styles from "./ImageSlider.module.css";
 
 const images = [
@@ -24,16 +24,23 @@ function ImageSlider() {
   const [timer, setTimer] = useState();
   const [currentIndex, setCurrentIndex] = useState(0);
   const [isSliderStop, setIsSliderStop] = useState(false);
+  const [currentPosition, setCurrentPosition] = useState(null);
+  const refSliderContainer = useRef();
 
   useEffect(() => {
     const autoSlide = async function () {
       if (currentIndex < images.length) {
-        setTimer(
-          setTimeout(() => {
-            setCurrentIndex((preState) => preState + 1);
-            setSliderXPosition((prevState) => prevState - 100);
-          }, 3000)
-        );
+        if (currentIndex < 0) {
+          setCurrentIndex(images.length - 1);
+          setSliderXPosition((images.length - 1) * -100);
+        } else {
+          setTimer(
+            setTimeout(() => {
+              setCurrentIndex((preState) => preState + 1);
+              setSliderXPosition((prevState) => prevState - 100);
+            }, 3000)
+          );
+        }
       } else {
         setCurrentIndex(0);
         setSliderXPosition(0);
@@ -49,14 +56,51 @@ function ImageSlider() {
     clearTimeout(timer);
   };
   const startSliderHandler = function () {
+    refSliderContainer.current.blur();
     setIsSliderStop(false);
   };
 
-  const sliderChangeHandler = function (e) {
-    const index = +e.target.getAttribute("index");
+  const sliderChangeHandler = function (e, to) {
     clearTimeout(timer);
-    setCurrentIndex(index);
-    setSliderXPosition(index * -100);
+
+    if (!to) {
+      const index = +e.target.getAttribute("index");
+      setCurrentIndex(index);
+      setSliderXPosition(index * -100);
+    }
+    if (to === "next") {
+      setCurrentIndex((prevState) => prevState + 1);
+      setSliderXPosition((prevState) => prevState - 100);
+    }
+    if (to === "prev") {
+      setCurrentIndex((prevState) => prevState - 1);
+      setSliderXPosition((prevState) => prevState + 100);
+    }
+  };
+  const startTouchHandler = function (e) {
+    const position = e.targetTouches[0].clientX;
+    setCurrentPosition(position);
+  };
+
+  const endTouchHandler = function (e) {
+    //在mobile上在touch/click結束時將不會把focus留在element上，這樣便不會觸發onMouseEnter
+    e.preventDefault();
+    startSliderHandler();
+  };
+
+  const moveTouchHandler = function (e) {
+    if (!currentPosition) return;
+    const movedPosition = e.targetTouches[0].clientX;
+    const diffDistance = Math.floor(movedPosition - currentPosition);
+
+    if (diffDistance < -50) {
+      sliderChangeHandler(null, "next");
+      setCurrentPosition(null);
+    }
+    if (diffDistance > 50) {
+      setCurrentPosition(null);
+      sliderChangeHandler(null, "prev");
+    }
   };
 
   const sliderImage = images.map((img, i) => {
@@ -89,7 +133,13 @@ function ImageSlider() {
 
   return (
     <>
-      <div className={`${styles.sliders_container} center`}>
+      <div
+        onTouchStart={startTouchHandler}
+        onTouchMove={moveTouchHandler}
+        onTouchEnd={endTouchHandler}
+        ref={refSliderContainer}
+        className={`${styles.sliders_container} center`}
+      >
         <div
           className={styles.slider_box}
           style={{ transform: `translateX(${sliderXPosition}%)` }}
